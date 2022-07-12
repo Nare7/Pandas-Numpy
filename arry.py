1bbedced64d6ef236b921c5a84829a5ea1be4c46
Naren <jonznaren@gmail.com>
2022-07-12 18:49:08 -0500

    Agg: Inicio de git

---
+++ arry.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+lista = [1, 2 , 3, 4, 5, 6, 7, 8, 9]
+
+
+arr = np.array(lista)
+#type(arr)
+
+
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # matriz de 3 dimenciones
+matriz = np.array(matriz)
+
+print(matriz[1,1]) # NumPay: antes de la coma hace referencia a las filas despues columnas
+
+print(matriz[0:2,1:3])
+
+
+
